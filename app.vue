cb3cbb230e239993e9c0c0507ea9b55f75705571
Mostafa Said <mostafasaid1994@gmail.com>
2024-11-19 12:01:51 +0200

    exercise 1

--- app.vue
+++ app.vue
@@ -5,6 +5,6 @@
     class="text-center dark:text-gray-200 text-gray-800 absolute-center h-screen flex flex-col justify-center items-center"
   >
     <h1 class="font-semibold text-5xl text-primary">Nuxt Fundamentals Workshop</h1>
-    <p class="text-lg font-normal mt-3">Use the repo branches to navigate through the exercises</p>
+    <p class="text-lg font-normal mt-3">You already solved the exercise if you see this</p>
   </div>
 </template>
