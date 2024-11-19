538522f3bc993ada434f32d13a0f7da615449ea7
Mostafa Said <mostafasaid1994@gmail.com>
2024-11-19 11:53:04 +0200

    init

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="text-center dark:text-gray-200 text-gray-800 absolute-center h-screen flex flex-col justify-center items-center"
+  >
+    <h1 class="font-semibold text-5xl text-primary">Nuxt Fundamentals Workshop</h1>
+    <p class="text-lg font-normal mt-3">Use the repo branches to navigate through the exercises</p>
+  </div>
+</template>
